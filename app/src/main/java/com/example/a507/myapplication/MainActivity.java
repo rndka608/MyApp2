a737dcc715cf89d0e645e400385896b5cbd7b737
rndka608 <rndka608@naver.com>
2017-09-07 10:52:01 +0900

    inital commit

--- app/src/main/java/com/example/a507/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a507/myapplication/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
  protected  Button btCount;
     protected TextView tvCount;
+    protected int nCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
         btCount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                nCount++;
+                String sCount = String.format("Count = %d", nCount);
+                tvCount.setText(sCount);
+                Toast.makeText(getBaseContext(), sCount, Toast.LENGTH_SHORT).show();
             }
         });
+        tvCount = (TextView) findViewById(R.id.tvCount);
 
-
-
+        nCount = 0;
 
     }
 }
